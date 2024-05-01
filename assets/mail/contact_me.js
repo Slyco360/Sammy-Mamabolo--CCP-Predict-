b63a529554a38067f6ec8b8608235852b251aee4
Slyco360 <167719264+Slyco360@users.noreply.github.com>
2024-05-01 15:06:12 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: https://vbzf76ahqj.execute-api.eu-west-1.amazonaws.com/2401FTDE-MOKMAM-CC-Predict-Lambda,
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
